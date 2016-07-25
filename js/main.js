5ea32d0b1e6da6a4e3f7b3455fd052fe0f79f6d6
goforu <99goforu@gmail.com>
2016-07-25 17:57:38 +0800

    d

--- js/main.js
+++ js/main.js
@@ -1,10 +1,55 @@
-(function ($){
-    $(window).on('load',function(){
-        var baloon = $('.btn-floating');
-        function runIt() {
-            baloon.animate({top:'+=10'}, 500);
-            baloon.animate({top:'-=10'}, 500, runIt);
-        }
-        runIt();
-    })
+(function ($) {
+    //$.material.init();
+    $(window).on('load', function () {
+        //var baloon = $('.btn-floating');
+        //function runIt() {
+        //    baloon.animate({top:'+=20'}, 500);
+        //    baloon.animate({top:'-=20'}, 500, runIt);
+        //}
+        //runIt();
+    });
+    $(document).ready(function () {
+        $(".button-collapse").sideNav();
+        $('.collapsible').collapsible();
+        registerFire();
+        registerSmoothScroll();
+        new WOW().init();
+        //$('.wrap-greeting-content').addClass('animated fadeInUp');
+    });
+
+
+    //register trigger fire
+    function registerFire() {
+        var options = [
+            {
+                selector: '.determinate', offset: 100, callback: function (el) {
+                skillAnim(el);
+            }
+            }
+        ];
+        Materialize.scrollFire(options);
+    }
+
+    //skill anim
+    function skillAnim(el) {
+        var percent = $(el).attr('data');
+        $(el).animate({
+            width: percent
+        }, 1000, "linear", function () {
+            $(el).find('.badge-tooltips').animate({
+                opacity: 1
+            }, 500);
+        });
+    }
+
+    //smooth scroll
+    function registerSmoothScroll() {
+        $('a').click(function (event) {
+            event.preventDefault();
+            var link = this;
+            $.smoothScroll({
+                scrollTarget: link.hash
+            });
+        })
+    }
 })(jQuery);
